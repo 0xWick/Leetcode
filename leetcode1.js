cc71e5c4e9d81a2c5bcab27552a598d82404f15b
Polymath <wickprof@gmail.com>
2022-12-15 11:01:39 +0500

    Improved

--- leetcode1.js
+++ leetcode1.js
@@ -1,30 +1,13 @@
 var runningSum = function(nums) {
-    
     const addedArray = []
-    console.log(nums)
-    
-    // * Iterate on each value in array
     for (let i = 0; i < nums.length; i++) {
         let AddNum = 0
-        // * Push 1st value normally
-        if (i == 0) {
-            addedArray.push(nums[i])
-        }
-        // * For Rest Values
-        else {
-        //    [1,2,3,4]     // Output: [1,3,6,10]
-        //     0 1 2 3      
-                //     i =  1
-                //     j =  1
-            for (let j = 0; j <= i ; j++) {
-                
+            for (let j = 0; j <= i; j++) {
                 AddNum += nums[j]
-        
             }
             addedArray.push(AddNum)
         }
-    }
-    console.log(addedArray)
+    return addedArray;
 };
-runningSum([1,1,1,1,1])
+// console.log(runningSum([1,1,1,1,1,1,1,1]))
 
